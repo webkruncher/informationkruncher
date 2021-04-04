213ffe9b8fefc03f7aa5bf84113ca9623a2b2d14
Jack M. Thompson <WebKruncher@GMail.com>
2021-04-04 13:43:07 -0500

    shared memory tools

--- include/shmemtools.h
+++ include/shmemtools.h
@@ -34,30 +34,6 @@
 
 namespace InfoTools
 {
-
-#if 0
-	#define MAX_LEN 10000
-	struct region {        /* Defines "structure" of shared memory */
-	    int len;
-	    char buf[MAX_LEN];
-	};
-	struct region *rptr;
-	int fd;
-	fd = shm_open("/myregion", O_CREAT | O_RDWR, S_IRUSR | S_IWUSR);
-	if (fd == -1)
-	    /* Handle error */;
-	if (ftruncate(fd, sizeof(struct region)) == -1)
-	    /* Handle error */;
-	/* Map shared memory object */
-	rptr = mmap(NULL, sizeof(struct region),
-	       PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-	if (rptr == MAP_FAILED)
-	    /* Handle error */;
-	   //rptr->len */
-#endif
-
-
-
 	template < typename T >
 		struct ShMem 
 	{
